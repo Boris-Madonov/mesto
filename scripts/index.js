26d0ddb9ecc356a2705a75195d3659248f862113
Boris Madonov <maboig@yandex.ru>
2020-07-05 22:28:39 +0300

    feat: добавить кнопку удаления карточки, шаблон для создания карточек, попапы для карточек. Реализовать открытие и закрытие разных попапов

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,28 @@
 // объявляем переменные
 
-let content = document.querySelector('.content');
-let popup = content.querySelector('.popup');
-let editProfileTextButton = content.querySelector('.profile__text-edit');
-let closePopupButton = popup.querySelector('.popup__close-button');
-let inputName = popup.querySelector('.popup__entry-field_account-name');
-let inputDescription = popup.querySelector('.popup__entry-field_account-description');
-let profileName = content.querySelector('.profile__text-name');
-let profileDescription = content.querySelector('.profile__text-description');
-let formElement = popup.querySelector('.popup__container');
+const content = document.querySelector('.content');
+const popup = content.querySelector('.popup');
+const inputName = popup.querySelector('.popup__entry-field_account-name');
+const inputDescription = popup.querySelector('.popup__entry-field_account-description');
+const profileName = content.querySelector('.profile__text-name');
+const profileDescription = content.querySelector('.profile__text-description');
+const formElement = popup.querySelector('.popup__container');
+
+const popupEditProfileText = content.querySelector('.popup__edit-profile');
+const popupNewItemAdd = content.querySelector('.popup__new-item');
+
+const editProfileTextButton = content.querySelector('.profile__text-edit');
+const addNewItemButton = content.querySelector('.profile__add-button');
+
+const closePopupEditProfileTextButton = popupEditProfileText.querySelector('.popup__close-button');
+const closePopupNewItemAddButton = popupNewItemAdd.querySelector('.popup__close-button');
+const closePopupImageButton = content.querySelector('.popup__close-button');
 
 // функция для открытия секции popup
-function popupOpen () {
+function popupOpen (popupType) {
 
     // добавляем модификатор для открытия секции popup
-    popup.classList.add('popup_opened');
+    popupType.classList.add('popup_opened');
 
     // при открытии секции popup в поля ввода переносится текущий текст
     inputName.value = profileName.textContent;
@@ -22,11 +30,12 @@ function popupOpen () {
 };
 
 // функция для закрятия секции popup
-function popupClose () {
+function popupClose (popupType) {
 
     // удаляем модификатор для открытия секции popup
-    popup.classList.remove('popup_opened');
+    popupType.classList.remove('popup_opened');  
 };
+
 
 // функция для переноса нового текста, при внесении изменений в текстовые поля в секции popup
 function formSubmitHandler (evt) {
@@ -38,15 +47,33 @@ function formSubmitHandler (evt) {
     profileName.textContent = inputName.value;
     profileDescription.textContent = inputDescription.value;
 
-    // при выполнении даннйо функции секция popup должна закрываться
-    popupClose ();
+    // при выполнении данной функции секция popup должна закрываться
+    popupClose (popupEditProfileText);
 };
 
+
+
 // открываем секцию popup при нажатии кнопки
-editProfileTextButton.addEventListener('click', popupOpen);
+editProfileTextButton.addEventListener('click', function () {
+    popupOpen (popupEditProfileText);
+});
+
+addNewItemButton.addEventListener('click', function () {
+    popupOpen (popupNewItemAdd);
+});
+
+
 
 // закрываем секцию popup при нажатии кнопки
-closePopupButton.addEventListener('click', popupClose);
+closePopupEditProfileTextButton.addEventListener('click', function () {
+    popupClose (popupEditProfileText);
+});
+
+closePopupNewItemAddButton.addEventListener('click', function () {
+    popupClose (popupNewItemAdd);
+});
+
+
 
 // заменяем текст и закрываем секцию popup при нажатии кнопки
 formElement.addEventListener('submit', formSubmitHandler);
