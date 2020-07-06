84434bdc02830200206edf8e5c4859b1034a822b
Boris Madonov <maboig@yandex.ru>
2020-07-06 22:18:18 +0300

    feat: оптимизировать код JS для открытия и закрытия разных попапов

--- scripts/index.js
+++ scripts/index.js
@@ -1,79 +1,106 @@
 // объявляем переменные
 
-const content = document.querySelector('.content');
-const popup = content.querySelector('.popup');
-const inputName = popup.querySelector('.popup__entry-field_account-name');
-const inputDescription = popup.querySelector('.popup__entry-field_account-description');
-const profileName = content.querySelector('.profile__text-name');
-const profileDescription = content.querySelector('.profile__text-description');
-const formElement = popup.querySelector('.popup__container');
+const content = document.querySelector('.content'); // контент
+const popup = content.querySelector('.popup'); // попап
 
-const popupEditProfileText = content.querySelector('.popup__edit-profile');
-const popupNewItemAdd = content.querySelector('.popup__new-item');
+const popupProfile = content.querySelector('.popup__edit-profile'); // попап с редактированием данных профиля
+const popupNewItem = content.querySelector('.popup__new-item'); // попап с добавлением новой карточки
+const popupImage = content.querySelector('.popup__image') // попап с открытие картинки
 
-const editProfileTextButton = content.querySelector('.profile__text-edit');
-const addNewItemButton = content.querySelector('.profile__add-button');
+const inputName = popupProfile.querySelector('.popup__entry-field_account-name'); // поле ввода имени в профайле
+const inputDescription = popupProfile.querySelector('.popup__entry-field_account-description'); // поле ввода описания в профайле
+const profileName = content.querySelector('.profile__text-name'); // текст в HTML в имени в профайле
+const profileDescription = content.querySelector('.profile__text-description'); // текст в HTML в описании в профайле
 
-const closePopupEditProfileTextButton = popupEditProfileText.querySelector('.popup__close-button');
-const closePopupNewItemAddButton = popupNewItemAdd.querySelector('.popup__close-button');
-const closePopupImageButton = content.querySelector('.popup__close-button');
+const formElement = popup.querySelector('.popup__container'); // форма в HTML в профайле
 
-// функция для открытия секции popup
-function popupOpen (popupType) {
+const openPopupProfileButton = content.querySelector('.profile__text-edit'); // кнопка открытия попапап профайл
+const openPopupNewItemButton = content.querySelector('.profile__add-button'); // кнопка открытия попапа новая карточка
 
-    // добавляем модификатор для открытия секции popup
-    popupType.classList.add('popup_opened');
+const closePopupProfileButton = popupProfile.querySelector('.popup__close-button'); // кнопка закрытия попапа профайла
+const closePopupNewItemButton = popupNewItem.querySelector('.popup__close-button'); // кнопка закрытия попапа новая карточка
+const closePopupImageButton = content.querySelector('.popup__close-button'); // кнопка закрытия попапа картинки
 
-    // при открытии секции popup в поля ввода переносится текущий текст
-    inputName.value = profileName.textContent;
-    inputDescription.value = profileDescription.textContent;
+
+// отсюда идет правильный код
+
+function popupToggle (popup) { // функция для открытия и закрытия попапа
+    popup.classList.toggle('popup_opened'); // добавляем/удаляем модификатор для открытия/закрытия попапа
 };
 
-// функция для закрятия секции popup
-function popupClose (popupType) {
-
-    // удаляем модификатор для открытия секции popup
-    popupType.classList.remove('popup_opened');  
-};
-
-
-// функция для переноса нового текста, при внесении изменений в текстовые поля в секции popup
-function formSubmitHandler (evt) {
-
-    // отменяем стандартную отправку формы, теперь можем определить свою логику отправки формы
-    evt.preventDefault();   
-
-    // вставляем в текстовые поля измененный текст в секции popup
-    profileName.textContent = inputName.value;
+const formSubmitHandler = function (evt) { // функция для переноса нового текста, при внесении изменений в текстовые поля в попапа
+    evt.preventDefault(); // отменяем стандартную отправку формы, теперь можем определить свою логику отправки формы
+    profileName.textContent = inputName.value;  // вставляем в текстовые поля измененный текст в секции popup
     profileDescription.textContent = inputDescription.value;
-
-    // при выполнении данной функции секция popup должна закрываться
-    popupClose (popupEditProfileText);
+    popupToggle(popupProfile);
 };
 
-
-
-// открываем секцию popup при нажатии кнопки
-editProfileTextButton.addEventListener('click', function () {
-    popupOpen (popupEditProfileText);
+openPopupProfileButton.addEventListener('click', function () { // открываем секцию popup при нажатии кнопки
+    popupToggle(popupProfile);
+    inputName.value = profileName.textContent; // при открытии секции popup в поля ввода переносится текущий текст
+    inputDescription.value = profileDescription.textContent;
 });
 
-addNewItemButton.addEventListener('click', function () {
-    popupOpen (popupNewItemAdd);
+openPopupNewItemButton.addEventListener('click', function () {
+    popupToggle(popupNewItem);
+});
+
+formElement.addEventListener('submit', formSubmitHandler);  // заменяем текст и закрываем секцию popup при нажатии кнопки
+
+closePopupProfileButton.addEventListener('click', function () { // закрываем секцию popup при нажатии кнопки
+    popupToggle(popupProfile);
+});
+
+closePopupNewItemButton.addEventListener('click', function () {
+    popupToggle(popupNewItem);
 });
 
 
+// ниже идет черновик
 
-// закрываем секцию popup при нажатии кнопки
-closePopupEditProfileTextButton.addEventListener('click', function () {
-    popupClose (popupEditProfileText);
-});
 
-closePopupNewItemAddButton.addEventListener('click', function () {
-    popupClose (popupNewItemAdd);
-});
+/* 
+
+
+buttonSavePopup.addEventListener('click', formSubmitHandler);
 
 
 
-// заменяем текст и закрываем секцию popup при нажатии кнопки
-formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+
+
+
+
+// создаем функционал удаления карточки по нажатию кнопки
+const itemsList = content.querySelector('.elements__list'); // связываем секцию с карточками
+const removeItemButton = itemsList.querySelectorAll('.element__remove'); // выбираем кнопку удаления карточки
+
+removeItemButton.addEventListener('click', function () {
+    const item = removeItemButton.closest('.element');
+    item.remove();
+  });
+
+// создаем функционал добавления новой карточки
+const inputItemName = popup.querySelector('.popup__entry-field_item-name');
+const inputItemImage = popup.querySelector('.popup__entry-field_item-image-url');
+
+
+// создаем связь шаблона для элементов (карточек) и наполняем его содержимым
+
+const itemTemplate = content.querySelector('.element-template'); // связываем шаблон для карточки
+
+function addNewItem (item) {
+
+    const newItem = itemTemplate.cloneNode(true); // создаем переменную (Новая карточка) и копируеим в нее шаблон
+
+// создаем поля ввода для занесения данных новой карточки
+
+
+// наполняем содержимым новую карточку по шаблону
+    newItem.querySelector('.element__image').src = item.link;
+    newItem.querySelector('.element__description').textContent = item.name;
+    itemsList.prepend(newItem);
+}
+*/
