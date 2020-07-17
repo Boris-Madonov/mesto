8563647a500219fd91c227102f9b1e0e693579fe
Boris Madonov <maboig@yandex.ru>
2020-07-17 16:58:36 +0300

    fix: исправить прменение объекта config

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,13 +1,11 @@
-
-
-const showInputError = (formSelector, inputElement, errorMessage) =>{               // функция показа ошибки валидации
+const showInputError = (config, formSelector, inputElement, errorMessage) =>{               // функция показа ошибки валидации
     const errorElement = formSelector.querySelector(`#${inputElement.id}-error`);   // выбираем элемент с ошибкой - у которого id заканчивается на -error
     inputElement.classList.add(config.inputElementErrorClass);                      // добавляем полю ввода класс для отображения ошибки
     errorElement.textContent = errorMessage;                                        // добавляем текст ошибки
     errorElement.classList.add(config.errorClass);                                  // добавляем стилизацию текста ошибки
 };
 
-const hideInputError = (formSelector, inputElement) => {                            // функция скрытия ошибки
+const hideInputError = (config, formSelector, inputElement) => {                            // функция скрытия ошибки
     const errorElement = formSelector.querySelector(`#${inputElement.id}-error`);   // выбираем элемент с ошибкой - у которого id заканчивается на -error
     inputElement.classList.remove(config.inputElementErrorClass);                   // удаляем стилизацию поля ввода для отображения ошибки
     errorElement.classList.remove(config.errorClass);                               // удаляем стилизацию текста ошибки
@@ -20,7 +18,7 @@ const hasInvalidInput = (inputList) => {                                        
     })
 };
 
-const toggleButtonState = (inputList, buttonElement) => {                           // функция переключения активности кнопки 'submit'
+const toggleButtonState = (config, inputList, buttonElement) => {                           // функция переключения активности кнопки 'submit'
     if (hasInvalidInput(inputList)) {                                               // условие если поле невалидное - кнопка должна быть не активной
         buttonElement.classList.add(config.inactiveButtonClass);
     } else {                                                                        // если нет не валидных полей - то кнопка должна быть активной
@@ -28,22 +26,22 @@ const toggleButtonState = (inputList, buttonElement) => {                       
     }
 };
 
-const isValid = (formSelector, inputElement) => {                                   // функция показа / скрытия ошибки валидации
+const isValid = (config, formSelector, inputElement) => {                                   // функция показа / скрытия ошибки валидации
     if (!inputElement.validity.valid) {                                             // условие если поле формы не валидно то запускаем функцию показа ошибки 
-        showInputError(formSelector, inputElement, inputElement.validationMessage);
-    } else {                                                                        // если поле валидно - то запускаем функцию скрятия ошибки
-        hideInputError(formSelector, inputElement);
+        showInputError(config, formSelector, inputElement, inputElement.validationMessage);
+    } else {                                                                        // если поле валидно - то запускаем функцию скрытия ошибки
+        hideInputError(config, formSelector, inputElement);
     }
 };
 
 const setEventListeners = (config, formSelector) => {                                       // слушатель по форме
     const inputList = Array.from(formSelector.querySelectorAll(config.inputElement));       // список (массив) всех полей в форме
     const buttonElement = formSelector.querySelector(config.submitButtonSelector);          // кнопка 'submit'
-    toggleButtonState(inputList, buttonElement);                                            // вызываем функцию переключения состояния кнопки 'submit' чтобы при открытии формы кнопка была неактивна
+    toggleButtonState(config, inputList, buttonElement);                                           // вызываем функцию переключения состояния кнопки 'submit' чтобы при открытии формы кнопка была неактивна
     inputList.forEach((inputElement) => {                                                   // для каждого поля списка запускаем слушатель по нажатию кнопки, тем самым проверяем валидность полей
         inputElement.addEventListener('input', () => {
-            isValid(formSelector, inputElement);
-            toggleButtonState(inputList, buttonElement);                                    // вызываем функцию переключения остояния кнопки 'submit' для ее включения в случае валидности всех полей
+            isValid(config, formSelector, inputElement);
+            toggleButtonState(config, inputList, buttonElement);                                    // вызываем функцию переключения остояния кнопки 'submit' для ее включения в случае валидности всех полей
         });
     });
 };
